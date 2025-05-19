a37e4c813fdae603dde0a88985d4c8e7978beac3
Duy-ruby <hoangduy@advn.vn>
2025-05-19 16:56:54 +0700

    refactor: combine browser anh é module type into index.js

--- README.md
+++ README.md
@@ -65,27 +65,27 @@ console.log(decodedCoordinates2); // Output: [ [ -25.42, -49.27 ], [ -25.41, -49
 ### Browser
 
 ```html
-<script src="https://unpkg.com/@trackasia/polyline"></script>
+<script src="https://unpkg.com/@trackasia/polyline/index.js"></script>
 <script>
 // Encode coordinates
 const coordinates = [[38.5, -120.2], [40.7, -120.95], [43.252, -126.453]];
 const encodedPolyline = polyline.encode(coordinates);
-console.log(encodedPolyline); // Output: _p~iF~ps|U_ulLnnqC_mqNvxq`@
+console.log(encodedPolyline);
 
 // Decode polyline
 const polylineString = '_p~iF~ps|U_ulLnnqC_mqNvxq`@';
 const decodedCoordinates = polyline.decode(polylineString);
-console.log(decodedCoordinates); // Output: [ [ 38.5, -120.2 ], [ 40.7, -120.95 ], [ 43.252, -126.453 ] ]
+console.log(decodedCoordinates);
 
 // Encode coordinates with precision 6
 const coordinates2 = [[-25.42, -49.27], [-25.41, -49.28], [-25.40, -49.29]];
 const encodedPolyline2 = polyline.encode(coordinates2, 6);
-console.log(encodedPolyline2); // Output: ~yczCn`vkHw}@nz}@oz}@n
+console.log(encodedPolyline2);
 
 // Decode polyline with precision 6
 const polylineString2 = '~yczCn`vkHw}@nz}@oz}@n';
 const decodedCoordinates2 = polyline.decode(polylineString2, 6);
-console.log(decodedCoordinates2); // Output: [ [ -25.42, -49.27 ], [ -25.41, -49.28 ], [ -25.4, -49.29 ] ]
+console.log(decodedCoordinates2);
 </script>
 ```
 
--- test/test.js
+++ test/test.js
@@ -1,68 +1,84 @@
 const assert = require('assert');
 const polyline = require('../index.js');
 
-describe('Polyline', function() {
-  it('should encode coordinates', function() {
-    const coordinates = [[38.5, -120.2], [40.7, -120.95], [43.252, -126.453]];
+describe('Polyline', function () {
+  it('should encode coordinates', function () {
+    const coordinates = [
+      [38.5, -120.2],
+      [40.7, -120.95],
+      [43.252, -126.453],
+    ];
     const encodedPolyline = polyline.encode(coordinates);
-    assert.equal(encodedPolyline, '_p~iF~ps|U_ulLnnqC_mqNvxq`@');
+    assert.strictEqual(encodedPolyline, '_p~iF~ps|U_ulLnnqC_mqNvxq`@');
   });
 
-  it('should decode polyline', function() {
+  it('should decode polyline', function () {
     const polylineString = '_p~iF~ps|U_ulLnnqC_mqNvxq`@';
     const decodedCoordinates = polyline.decode(polylineString);
-    assert.deepEqual(decodedCoordinates, [
-      [ 38.5, -120.2 ],
-      [ 40.7, -120.95 ],
-      [ 43.252, -126.453 ]
+    assert.deepStrictEqual(decodedCoordinates, [
+      [38.5, -120.2],
+      [40.7, -120.95],
+      [43.252, -126.453],
     ]);
   });
 
-  it('should encode coordinates 2 (precision 5)', function() {
-    const coordinates = [[-25.42, -49.27], [-25.41, -49.28], [-25.40, -49.29]];
+  it('should encode coordinates 2 (precision 5)', function () {
+    const coordinates = [
+      [-25.42, -49.27],
+      [-25.41, -49.28],
+      [-25.4, -49.29],
+    ];
     const encodedPolyline = polyline.encode(coordinates);
-    assert.equal(encodedPolyline, '~yczCn`vkHo}@n}@o}@n}@');
+    assert.strictEqual(encodedPolyline, '~yczCn`vkHo}@n}@o}@n}@');
   });
 
-  it('should decode polyline 2 (precision 5)', function() {
+  it('should decode polyline 2 (precision 5)', function () {
     const polylineString = '~yczCn`vkHo}@n}@o}@n}@';
     const decodedCoordinates = polyline.decode(polylineString);
-    assert.deepEqual(decodedCoordinates, [
-      [ -25.42, -49.27 ],
-      [ -25.41, -49.28 ],
-      [ -25.4, -49.29 ]
+    assert.deepStrictEqual(decodedCoordinates, [
+      [-25.42, -49.27],
+      [-25.41, -49.28],
+      [-25.4, -49.29],
     ]);
   });
 
-  it('should encode coordinates 2 (precision 6)', function() {
-    const coordinates = [[-25.42, -49.27], [-25.41, -49.28], [-25.40, -49.29]];
+  it('should encode coordinates 2 (precision 6)', function () {
+    const coordinates = [
+      [-25.42, -49.27],
+      [-25.41, -49.28],
+      [-25.4, -49.29],
+    ];
     const encodedPolyline = polyline.encode(coordinates, 6);
-    assert.equal(encodedPolyline, '~yczCn`vkHw}@nz}@oz}@n');
+    assert.strictEqual(encodedPolyline, '~lono@~me~|A_pR~oR_pR~oR');
   });
 
-  it('should decode polyline 2 (precision 6)', function() {
-    const polylineString = '~yczCn`vkHw}@nz}@oz}@n';
+  it('should decode polyline 2 (precision 6)', function () {
+    const polylineString = '~lono@~me~|A_pR~oR_pR~oR';
     const decodedCoordinates = polyline.decode(polylineString, 6);
-    assert.deepEqual(decodedCoordinates, [
-      [ -25.42, -49.27 ],
-      [ -25.41, -49.28 ],
-      [ -25.4, -49.29 ]
+    assert.deepStrictEqual(decodedCoordinates, [
+      [-25.42, -49.27],
+      [-25.41, -49.28],
+      [-25.4, -49.29],
     ]);
   });
 
-  it('should encode coordinates 3', function() {
-    const coordinates = [[0, 0], [10, 10], [20, 20]];
+  it('should encode coordinates 3', function () {
+    const coordinates = [
+      [0, 0],
+      [10, 10],
+      [20, 20],
+    ];
     const encodedPolyline = polyline.encode(coordinates);
-    assert.equal(encodedPolyline, '??_c`|@_c`|@_c`|@_c`|@');
+    assert.strictEqual(encodedPolyline, '??_c`|@_c`|@_c`|@_c`|@');
   });
 
-  it('should decode polyline 3', function() {
+  it('should decode polyline 3', function () {
     const polylineString = '??_c`|@_c`|@_c`|@_c`|@';
     const decodedCoordinates = polyline.decode(polylineString);
-    assert.deepEqual(decodedCoordinates, [
-      [ 0, 0 ],
-      [ 10, 10 ],
-      [ 20, 20 ]
+    assert.deepStrictEqual(decodedCoordinates, [
+      [0, 0],
+      [10, 10],
+      [20, 20],
     ]);
   });
 });
